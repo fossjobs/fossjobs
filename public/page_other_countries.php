395ff0d8d7dbe562f3e66ec112cae3d67fa26cb5
Waldir Pimenta <waldyrious@gmail.com>
2025-04-16 09:35:37 +0100

    Normalize HTML title and main heading of Worldwide/Remote jobs page

--- public/page_other_countries.php
+++ public/page_other_countries.php
@@ -1,20 +1,20 @@
 <?php
 	$type_id = get_type_id_by_varname($id);
-	
+
 	$jobCount =  $job->GetJobsCountForCountry(0, $type_id);
-	
+
 	$paginatorLink = BASE_URL . "jobs-worldwide";
-	
+
 	if (isset($extra))
 		$paginatorLink .= "/$extra";
-		
+
 	$paginator = new Paginator($jobCount, JOBS_PER_PAGE, @$_REQUEST['p']);
 	$paginator->setLink($paginatorLink);
 	$paginator->paginate();
-	
+
 	$firstLimit = $paginator->getFirstLimit();
 	$lastLimit = $paginator->getLastLimit();
-	
+
 	$the_jobs = array();
 	$the_jobs = $job->GetPaginatedJobsForOtherCountries($type_id, $firstLimit, JOBS_PER_PAGE);
 	$smarty->assign("pages", $paginator->pages_link);
@@ -22,9 +22,9 @@
 	$smarty->assign('jobs', $the_jobs);
 	$smarty->assign('types', get_types());
 
-	$smarty->assign('seo_title', 'Jobs in other countries');
+	$smarty->assign('seo_title', 'Worldwide/Remote jobs');
 	$smarty->assign('seo_desc', '');
 	$smarty->assign('seo_keys', '');
-	
+
 	$template = 'other-countries-jobs-header.tpl';
 ?>
